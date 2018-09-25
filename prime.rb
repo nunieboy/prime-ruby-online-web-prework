40e10d576ff05017f7da7857b26de1c5b045e775
Pablo Eusebio <nunieboy88@gmail.com>
2018-09-25 16:45:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,13 +1,15 @@
 # # Add  code here!
+require 'pry'
 
 def prime?(number)
   divisor = 0
-  for i in (1..number) do
+  for i in 1..number do
     prime_num = number % i
     if prime_num == 0
       divisor += 1
     end
-  if divisor == 1
+  end
+  if divisor == 2
     return true
   else
     return false
